c4c23599e7616b120083a9ad7e368f7b95849d35
gregoryhornyak <hornyakj@coventry.ac.uk>
2024-02-06 18:06:37 +0000

    beginning

---
+++ flask_app/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+app = Flask(__name__)
+
+#logging.basicConfig(filename="/home/ubuntu/flask_app/app.log", level=logging.DEBUG)
+
+dummy_user = {'username': 'lajos', 'password': 'jelszo'}
+
+@app.route('/')
+def hello():
+    return render_template('game.html')#"<h1>Hello there!</h1><br>login: /main<br>letterloopd: /loop"
+
+@app.route('/test')
+def test():
+    return render_template('test_letter.html')
+
+@app.route('/how')
+def structure():
+    return render_template('structure.txt')
+
+@app.route('/reboot')
+def restart():
+    os.system("sudo sh /home/ubuntu/upd_and_reboot_server.sh")
+    return 'restarting server'
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+@app.route('/loop')
+def rickroll():
+    return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    try:
+        username = request.form.get('username')
+        password = request.form.get('password')
+        app.logger.debug(f"{username = }:{password = }")
+        if username == dummy_user['username'] and password == dummy_user['password']:
+            # Successful login
+            return render_template('inside.html')
+        else:
+            # Failed login
+            return 'Invalid credentials. Please try again te balfasz.'
+    except Exception as e:
+        app.logger.error(f"An error occured: {str(e)}", exc_info=True)
+        return "Gebasz", 500
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ flask_app/app.wsgi
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0, '/var/www/html/flask_app')
+from app import app as application
