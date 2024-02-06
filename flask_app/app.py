c73b24daaac6fe54f6986eaa535510f0816a8494
gregoryhornyak <hornyakj@coventry.ac.uk>
2024-02-06 22:22:54 +0000

    pdf path

--- flask_app/app.py
+++ flask_app/app.py
@@ -89,7 +89,7 @@ def currentIssue(issueName):
 @app.route('/pdf')
 def render_pdf():
     # Path to your PDF file
-    pdf_file_path = 'eternal_sunshine_of_the_spotless_mind.pdf'#'/var/www/html/flask_app/templates/eternal_sunshine_of_the_spotless_mind.pdf'
+    pdf_file_path = 'templates/eternal_sunshine_of_the_spotless_mind.pdf'#'/var/www/html/flask_app/templates/eternal_sunshine_of_the_spotless_mind.pdf'
     
     # Render the PDF file
     return send_file(pdf_file_path, as_attachment=True)
